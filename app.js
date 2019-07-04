11719abf046a1cc1e14c7ed8986c129674d82770
Alcina <white_kaitou@hotmail.com>
2019-07-03 22:26:13 -0400

    Adding route and express

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-//require express in to run server
+//require express package
 const express = require('express');
 //call express 
 const app = express();
 
-//now have the ability to create routes!
+//ROUTES
+//can add, edit, post, etc. and get a response back ex. sending a form
+app.get('/', (request, response) => {
+    response.send('We are on home');
+});
+//listen to port 3000
+app.listen(3000);
 
