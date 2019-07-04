fe0a6aeb4d23db3500642bb55f897fbc85e31ca7
Alcina <white_kaitou@hotmail.com>
2019-07-04 01:04:39 -0400

    Testing post route

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const express = require('express');
 //call express 
 const app = express();
 
+//Middlewares
+//the function that executes when routes are being hit 
+// app.use('/posts', () => {
+//     console.log('Middleware running');
+// });
+
 //ROUTES
 //can add, edit, post, etc. and get a response back ex. sending a form
 app.get('/', (request, response) => {
     response.send('We are on home');
+});
+app.get('/posts', (request, response) => {
+    response.send('We are on posts');
 });
 //listen to port 3000
 app.listen(3000);
