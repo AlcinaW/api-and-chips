99d5ba3b3bf9d471bd854e003a2b8e04ccaa5c61
Alcina <white_kaitou@hotmail.com>
2019-07-23 21:55:50 -0400

    Added new schema and validation

--- authentication-with-jwt/routes/auth.js
+++ authentication-with-jwt/routes/auth.js
@@ -1,20 +1,34 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
+//Validation
+const Joi = require('@hapi/joi');
+
+//schema for elevation
+const schema = {
+    name: Joi.string().min(2).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+
+}
+
 //create empty router 
 router.post('/register', async (request, response) => {
+    //validate the data before making the user
+    const validation = Joi.validate(request.body, schema);
+    response.send(validation);
     //create new user with POST
-    const user = new User({
-        name: request.body.name,
-        email: request.body.email,
-        password: request.body.password
-    });
-    try {
-        const savedUser = await user.save();
-        response.send(savedUser);
-    } catch (err) {
-        response.status(400).send(err);
-    }
+    // const user = new User({
+    //     name: request.body.name,
+    //     email: request.body.email,
+    //     password: request.body.password
+    // });
+    // try {
+    //     const savedUser = await user.save();
+    //     response.send(savedUser);
+    // } catch (err) {
+    //     response.status(400).send(err);
+    // }
 
 });
 
