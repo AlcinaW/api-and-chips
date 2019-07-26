8a7f071160cec96c928c599d94a902e3c6b881bd
Alcina <white_kaitou@hotmail.com>
2019-07-26 01:31:05 -0400

    cleaning up exports and validation schemas

--- authentication-with-jwt/routes/auth.js
+++ authentication-with-jwt/routes/auth.js
@@ -1,27 +1,27 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
+
+
+
 //create empty router 
 router.post('/register', async (request, response) => {
-    //validate the data before making the user
+    //validate the data before we create a user
     const { error } = Joi.validate(request.body, schema);
-    //check in Postman what the object is with the errors
-    //show the error message
     if (error) return response.status(400).send(error.details[0].message);
 
-
     //create new user with POST
-    // const user = new User({
-    //     name: request.body.name,
-    //     email: request.body.email,
-    //     password: request.body.password
-    // });
-    // try {
-    //     const savedUser = await user.save();
-    //     response.send(savedUser);
-    // } catch (err) {
-    //     response.status(400).send(err);
-    // }
+    const user = new User({
+        name: request.body.name,
+        email: request.body.email,
+        password: request.body.password
+    });
+    try {
+        const savedUser = await user.save();
+        response.send(savedUser);
+    } catch (err) {
+        response.status(400).send(err);
+    }
 
 });
 
--- authentication-with-jwt/validation.js
+++ authentication-with-jwt/validation.js
@@ -3,11 +3,21 @@ const Joi = require('@hapi/joi');
 
 //Register validation
 //wrap in function
-const registerValidation = () => {
+const registerValidation = (data) => {
     const schema = {
-        name: Joi.string().min(2).required(),
-        email: Joi.string().min(6).required().email(),
-        password: Joi.string().min(6).required()
+        name: Joi.string()
+            .min(2)
+            .required(),
+        email: Joi.string()
+            .min(6)
+            .required()
+            .email(),
+        password: Joi.string()
+            .min(6)
+            .required()
 
     };
-}
+    return Joi.validate(data, schema);
+};
+
+module.exports.registerValidation = registerValidation;
