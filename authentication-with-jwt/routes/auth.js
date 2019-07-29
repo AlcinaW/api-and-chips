efc94a9cfb62bb7a8b8c64c2c55847e6e7815ea6
Alcina <white_kaitou@hotmail.com>
2019-07-28 23:05:41 -0400

    Validation if user already exists

--- authentication-with-jwt/routes/auth.js
+++ authentication-with-jwt/routes/auth.js
@@ -1,14 +1,18 @@
 const router = require('express').Router();
 const User = require('../model/User');
-
+const { registerValidation } = require('../validation');
 
 
 
 //create empty router 
 router.post('/register', async (request, response) => {
     //validate the data before we create a user
-    const { error } = Joi.validate(request.body, schema);
+    const { error } = registerValidation(request.body);
     if (error) return response.status(400).send(error.details[0].message);
+
+    //check if user is already in database
+    const emailExist = await User.findOne({ email: request.body.email });
+    if (emailExist) return response.status(400).send('Email already exists');
 
     //create new user with POST
     const user = new User({
