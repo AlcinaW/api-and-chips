459d43de3b04295dc09cc666a022cd224bd5f4be
Alcina <white_kaitou@hotmail.com>
2019-08-11 21:37:47 -0400

    Adding CORS middleware and public directory

--- real-time-polling/app.js
+++ real-time-polling/app.js
@@ -6,3 +6,17 @@ const cors = require('cors');
 const app = express();
 
 //set public folder 
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ encoded: false }));
+
+//enable CORS
+app.use(cors());
+
+//create port number
+const port = 3000;
+
+//start server 
+app.listen();
