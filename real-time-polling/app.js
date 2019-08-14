0f8300fff6efcaf2452f1a28aa6987731c6a1332
Alcina <white_kaitou@hotmail.com>
2019-08-13 23:49:24 -0400

    start poll routing

--- real-time-polling/app.js
+++ real-time-polling/app.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 
 const app = express();
 
+const poll = require('./routes/poll');
+
 //set public folder 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -14,6 +16,8 @@ app.use(bodyParser.urlencoded({ encoded: false }));
 
 //enable CORS
 app.use(cors());
+
+app.use('/poll', poll);
 
 //create port number
 const port = 3000;
