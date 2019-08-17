f772beaebb385bbe69aae7c713b53d529233f01c
Alcina <white_kaitou@hotmail.com>
2019-08-16 21:36:52 -0400

    starting router post

--- real-time-polling/routes/poll.js
+++ real-time-polling/routes/poll.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const pusher = require('pusher');
+
+//put key here
+
 router.get('/', (request, response) => {
     response.send('Poll');
+});
+
+router.post('/', (request, response) => {
+
 });
 
 module.exports = router;
