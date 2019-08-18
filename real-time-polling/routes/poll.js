f6cc06aeec667b3a0d450f8b04c6e5becd3faf04
Alcina <white_kaitou@hotmail.com>
2019-08-18 15:33:31 -0400

    Add pusher trigger and Materialize CSS inputs

--- real-time-polling/routes/poll.js
+++ real-time-polling/routes/poll.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const dotenv = require('dotenv').config();
 
-const pusher = require('pusher');
+const Pusher = require('pusher');
 
 //put key here
+let channels_client = new Pusher({
+    appId: process.env.PUSHER_ID,
+    key: process.env.PUSHER_KEY,
+    secret: process.env.PUSHER_KEY,
+    cluster: 'us2',
+    encrypted: true
+});
 
 router.get('/', (request, response) => {
     response.send('Poll');
 });
 
 router.post('/', (request, response) => {
-
+    channels_client.trigger('os-poll', 'os-vote', {
+        points: 1,
+        os: request.body.os
+    });
+    return response.json({ success: true, message: 'Thank you for voting' })
 });
 
 module.exports = router;
